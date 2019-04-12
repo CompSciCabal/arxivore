cc079f2bb5e8527683ed20c1117397636c4c5ef4
inaimathi <leo.zovic@gmail.com>
2019-04-12 11:15:50 -0400

    Handle multi-page results in get-historic-paper-urls

--- src/cljoth/core.clj
+++ src/cljoth/core.clj
@@ -2,6 +2,7 @@
   (:require [clojure.xml :as xml]
             [clojure.string :as str]
             [clojure.java.io :as io]
+            [clojure.edn :as edn]
 
             [environ.core :as environ]
             [org.httpkit.client :as http]
@@ -23,6 +24,13 @@
   (env :cljoth-papers
        :default (str (System/getProperty "user.home") "/cljoth-papers/")))
 
+(defn get! [url]
+  (Thread/sleep 1000)
+  (:body @(http/get url)))
+
+(defn get-resource! [url]
+  (html/html-resource (java.io.StringReader. (get! url))))
+
 (defn paper-urls-in [url]
   (->> (xml/parse url)
        :content first :content
@@ -38,25 +46,34 @@
     (drop 6 (map (fn [a b] [a b])
                  dates (rest dates)))))
 
-(defn -format-query [[[y1 m1] [y2 m2]]]
-  (format "https://arxiv.org/search/advanced?advanced=1&terms-0-operator=AND&terms-0-term=&terms-0-field=title&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=%d-%02d&date-to_date=%d-%02d&date-date_type=submitted_date&abstracts=show&size=200&order=-announced_date_first"
-          y1 m1 y2 m2))
+(defn -format-query [[[y1 m1] [y2 m2]] & {:keys [start]}]
+  (let [fmt (format "https://arxiv.org/search/advanced?advanced=1&terms-0-operator=AND&terms-0-term=&terms-0-field=title&classification-computer_science=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=%d-%02d&date-to_date=%d-%02d&date-date_type=submitted_date&abstracts=show&size=200&order=-announced_date_first"
+                    y1 m1 y2 m2)]
+    (if start
+      (str fmt "&start=" start)
+      fmt)))
 
-(defn -urls-from-result-page [pg]
-  (map
-   #(-> % :content first :attrs :href)
-   (html/select
-    (html/html-resource (java.io.StringReader. pg))
-    [:li.arxiv-result :p.list-title])))
+(defn -urls-from-single-page [resource]
+  (map #(-> % :content first :attrs :href)
+       (html/select
+        resource
+        [:li.arxiv-result :p.list-title])))
 
-(defn get! [url]
-  (Thread/sleep 1000)
-  (:body @(http/get url)))
+(defn -urls-from-date-range [date-range]
+  (let [resource (get-resource! (-format-query date-range))
+        title (-> (html/select resource [:h1]) first :content first)]
+    (if-let [match (rest (re-find #"Showing (\d+)[–-](\d+) of ([\d,]+)" title))]
+      (let [[from to of] (map #(edn/read-string (str/replace % #"," "")) match)]
+        (mapcat
+         #(-urls-from-single-page
+           (get-resource!
+            (-format-query
+             date-range :start %)))
+         (range to of to)))
+      (-urls-from-single-page resource))))
 
 (defn historic-paper-urls []
-  (mapcat
-   #(-urls-from-result-page (get! (-format-query %)))
-   (-all-date-ranges)))
+  (mapcat -urls-from-date-range (-all-date-ranges)))
 
 (defn pdf-path [paper-url]
   (let [id (last (str/split paper-url #"/"))]
